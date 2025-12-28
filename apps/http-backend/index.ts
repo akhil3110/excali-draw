2bb6d72a6461188d1468bb6942ed64e7535ac7f9
Akhil <akhil1659@gmail.com>
2025-12-28 16:08:22 +0530

    http backend enpoint added

--- apps/http-backend/index.ts
+++ apps/http-backend/index.ts
@@ -1,13 +1,48 @@
 import express from "express"
+import cors from "cors"
+import jwt from "jsonwebtoken";
+import { JWT_SECERET } from "./config";
+import { middleware } from "./middleware";
 
 const app = express();
 const port = 3001;
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/",(req,res) => {
+
     res.send("Hello world")
+    
 })
+
+app.put("/sign-in",(req,res) => {
+    const {email, password} = req.body
+
+    const userId = 1;
+    const token  = jwt.sign({
+        userId
+    },JWT_SECERET)
+
+    res.json({token})
+})
+
+app.post("/sign-up",(req,res) => {
+    res.json({
+        userId: 123
+    })
+})
+
+
+app.post("/create-room",middleware,(req,res) => {
+    //db  call create a room
+
+    res.json({
+        roomId: 1
+    })
+})
+
+
 
 app.listen(port, () => {
     console.log("Backend App started at port " +port )
---
+++ apps/http-backend/middleware.ts
@@ -0,0 +1,20 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken"
+import { JWT_SECERET } from "./config";
+
+
+export function middleware(req: Request,res: Response,next: NextFunction){
+
+    const token = req.headers["authorization"] ?? ""  
+
+    const decoded = jwt.verify(token,JWT_SECERET) as JwtPayload & {userId: string}
+
+    if(decoded)  {
+        req.userId = decoded.userId;
+        next()
+    } else {
+        res.status(403).json({
+            message: "Unauthorized"
+        })
+    }
+} 
