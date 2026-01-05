32f4a0d4a1b1edb737af39080128fc462662ee26
Akhil <akhil1659@gmail.com>
2026-01-05 16:33:06 +0530

    ws inital message transfer implementation completed

--- apps/ws-backend/index.ts
+++ apps/ws-backend/index.ts
@@ -1,9 +1,32 @@
-import { WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer } from 'ws';
 import jwt, { type JwtPayload } from "jsonwebtoken"
 import { JWT_SECERET } from '@repo/backend-common/secret';
 
 const wss = new WebSocketServer({ port: 8080 });
 
+interface User {
+    ws: WebSocket
+    rooms: string[],
+    userId: string
+}
+
+const users: User[] = []
+
+
+
+function CheckUser (token: string) {
+    try {
+        const decoded = jwt.verify(token,JWT_SECERET)
+        if(!decoded){
+            return null;
+        }
+    
+        return decoded
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
 
 wss.on('connection', function connection(ws,request) {
 
@@ -16,26 +39,79 @@ wss.on('connection', function connection(ws,request) {
 
     const queryparams = new URLSearchParams(url.split('?')[1])
     const token  = queryparams.get("token")
-    console.log("Token:",token)
 
+    console.log(token)
     if(!token){
         ws.close();
         return
     }
     
-    const decoded = jwt.verify(token,JWT_SECERET)
-    console.log("Decoded token:",decoded)
-    if(!decoded){
-        ws.close();
+    console.log("ADD")
+    const UserId = CheckUser(token)
+    if(!UserId){
+        console.log("user Disconected")
+        ws.close()
         return
     }
+
+    users.push({
+        userId: UserId.toString(),
+        rooms: [],
+        ws
+    })
+    console.log(users)
 
     ws.on('error', console.error);
 
     console.log("Websocket server connected")
 
     ws.on('message', function message(data) {
-        console.log('received: %s', data);
+
+        const parsedData = JSON.parse(data as unknown as string);
+        console.log(parsedData)
+
+//         {
+//              type: "join_room",
+//              roomId: 1
+//          }
+        if(parsedData.type === "join_room"){
+            users.find((u) =>  u.ws === ws)?.rooms.push(parsedData.roomId)
+            ws.send("User %s" + UserId +" joined "+ parsedData.roomId)
+            
+        }
+
+        if (parsedData.type === "leave_room") {
+            const user = users.find(u => u.ws === ws)
+            if (!user) return
+
+            user.rooms = user.rooms.filter(
+                roomId => roomId !== parsedData.roomId
+            )
+        }
+
+
+        // {
+        //     "type": "chat",
+        //     "roomId": 1,
+        //     "message": "Hello everyone"
+        // }
+        if(parsedData.type === "chat"){
+            const roomId  = parsedData.roomId
+            const message = parsedData.message
+
+            users.forEach((u) => {
+                if(u.rooms.includes(roomId)){
+                    // u.ws.send(JSON.stringify({
+                    //     type: "chat",
+                    //     roomId,
+                    //     message,
+                    // }))
+                    u.ws.send( message)
+                }
+            })
+        }
+        
+
         ws.send(data.toString());
 
     });
