941b97bb0313b05165481e3eae61fd57ac605a36
Akhil <akhil1659@gmail.com>
2026-03-06 16:44:19 +0530

    removed web

--- packages/redis/client.ts
+++ packages/redis/client.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 import Redis from "ioredis";
 
-
+console.log("Redis URL:", process.env.REDIS_URL);
 export const redisPublisher = new Redis(process.env.REDIS_URL!);
 export const redisConsumer =new Redis(process.env.REDIS_URL!);
 
